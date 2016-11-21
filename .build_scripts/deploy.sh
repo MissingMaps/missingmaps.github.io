6aa73628b02e731aa34e8138a19b549aad5f09cd
Marc Farra <marcfarra@gmail.com>
2016-11-21 14:47:19 -0500

    Discard Travis output

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -10,7 +10,7 @@ if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; 
   git config user.email "travis@somewhere.com"
   git add .
   git commit -m "CI deploy to gh-pages"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
 else
   echo "Not a publishable branch so we're all done here"
 fi
