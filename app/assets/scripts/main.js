8050b87506b66598782f82e3379bd0cc596538af
Seth Fitzsimmons <seth@mojodna.net>
2018-03-21 18:17:37 -0700

    Drop priorStats
    Now that we have a full dataset, we no longer need to adjust stats by
    adding "priorStats".

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,24 +1,7 @@
 /*
 Populates contribution statistics on landing page using
 the root Missing Maps API endpoint.
-
-Note: MissingMap began collecting statistics in realtime
-beginning February 16, 2016, a few of which are displayed
-on the MissingMaps landing page (users, total edits,
-building edits, and road kilometer additions). The object
-below contains a summary of the number of edits in these
-categories that were submitted prior to February 16, 2016,
-which are added to the running total to estimate totals
-going back to 2014. Users are calculated in realtime by the
-current API, so no additions are necessary.
 */
-
-const priorStats = {
-	users: 0,
-	edits: 22378685,
-	buildings: 2435322,
-	roads: 215683
-};
 
 const xmlhttp = new XMLHttpRequest();
 const url = 'https://osm-stats-production-api.azurewebsites.net/stats/missingmaps';
@@ -34,8 +17,7 @@ xmlhttp.send();
 
 function updateStatistics (stats) {
   ['users', 'edits', 'buildings', 'roads'].forEach(function (cat) {
-  	const total = Math.floor(priorStats[cat] + stats[cat]);
     document.getElementById('stats-' + cat + 'Count')
-      .innerHTML = total.toLocaleString();
+      .innerHTML = stats[cat].toLocaleString();
   });
 };
