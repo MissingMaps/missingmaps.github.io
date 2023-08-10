a18df7ce6434e4f11f331bd655d91789fc84c816
jstier <stier.jochen@web.de>
2023-08-10 11:24:06 +0200

    switched to correct url and ohsome format

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -4,7 +4,7 @@ the root Missing Maps API endpoint.
 */
 
 const xmlhttp = new XMLHttpRequest();
-const url = "https://stats.now.ohsome.org/api";
+const url = "https://stats.now.ohsome.org/api/stats/missingmaps?ohsomeFormat=true";
 
 xmlhttp.onreadystatechange = function() {
   if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
@@ -18,7 +18,7 @@ xmlhttp.send();
 function updateStatistics(stats) {
   ["users", "edits", "buildings", "roads"].forEach(function(cat) {
     document.getElementById("stats-" + cat + "Count").innerHTML = Math.round(
-      stats[cat]
+      stats.result[cat]
     ).toLocaleString();
   });
 }
